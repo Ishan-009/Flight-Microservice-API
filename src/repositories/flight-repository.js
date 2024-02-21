2bfd47e6cd01159f8521774bbd729f5bb9e81e1c
Ishan-009 <ishanmoorjmalani009@gmail.com>
2024-02-21 20:05:12 +0530

    FEAT:- Implemented Multiple Joins on Various Models for query generation with more details

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,5 +1,6 @@
 const CrudRepository = require("./crud-repository");
-const { Flight } = require("../models");
+const { Flight, Airplane, Airport, City } = require("../models");
+const { Sequelize } = require("sequelize");
 
 class FlightRepository extends CrudRepository {
   constructor() {
@@ -10,8 +11,57 @@ class FlightRepository extends CrudRepository {
     const response = await Flight.findAll({
       where: filter,
       order: sort,
+      include: [
+        {
+          model: Airplane,
+          as: "airplane_details",
+
+          requried: true,
+        },
+        {
+          model: Airport,
+          required: true,
+          as: "departureAirport",
+
+          on: {
+            col1: Sequelize.where(
+              Sequelize.col("Flight.departureAirportId"),
+              "=",
+              Sequelize.col("departureAirport.code")
+            ),
+          },
+
+          include: {
+            model: City,
+            required: true,
+          },
+        },
+
+        {
+          model: Airport,
+          required: true,
+          as: "arrivalAirport",
+
+          on: {
+            col1: Sequelize.where(
+              Sequelize.col("Flight.arrivalAirportId"),
+              "=",
+              Sequelize.col("arrivalAirport.code")
+            ),
+          },
+
+          include: {
+            model: City,
+            required: true,
+          },
+        },
+      ],
     });
-    console.log("flight repository", response);
+    // IMP Notes
+    // here we have to put alias in both model and repository in order to use alias , also here we have to mention the col name in which we have to apply inner join on whihc is code here by default it will take airport It so ussing sequelize library we are using col method to specify the query to use arrival adn departure airprot id to code as we are storing code in these both fields
+
+    // here we are using alias as refernce to implement things or access things like "Flight.arrivalAirportId" here "Flight " is an alias same goes for "arrivalAirport" and "departureAirport", it requires so it could figure out on which association it want to impolement operation as "Airport.code" could be use multiple times as it could figure on which column it want to apply implementation
+
     return response;
   }
 }
