6cc653477928d02f2e353fb19d5d3c7a7c2fec53
Ishan-009 <ishanmoorjmalani009@gmail.com>
2024-02-29 14:14:11 +0530

    Implemented Reverse Proxy test log on controller

--- src/controller/info-controller.js
+++ src/controller/info-controller.js
@@ -1,5 +1,7 @@
 const { StatusCodes } = require("http-status-codes");
 const info = (req, res) => {
+  console.log("Recieved Request inside flight service");
+
   return res.status(StatusCodes.OK).json({
     success: true,
     message: "API is live",
